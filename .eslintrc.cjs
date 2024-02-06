0a9b0608c776770b11922a2e94e9abd7e3677b14
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2024-02-06 19:24:32 +0300

    add images and fonts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,61 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    "browser": true,
+    "es2020": true,
+    "jest": true,
+    "node": true
   },
+  settings: {
+    "react": {
+      "version": "detect"
+    }
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:tailwindcss/recommended"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 11,
+    sourceType: "module"
+  },
+  plugins: ["react", "react-hooks", "@typescript-eslint", "tailwindcss"],
+  rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "tailwindcss/classnames-order": "warn",
+    "tailwindcss/no-custom-classname": "warn",
+    "tailwindcss/no-contradicting-classname": "error"
+  }
+  // root: true,
+  // env: { browser: true, es2020: true },
+  // extends: [
+  //   'eslint:recommended',
+  //   'plugin:react/recommended',
+  //   'plugin:react/jsx-runtime',
+  //   'plugin:react-hooks/recommended',
+  //   "plugin:prettier/recommended",
+  //   "plugin:tailwindcss/recommended"
+  // ],
+  // ignorePatterns: ['dist', '.eslintrc.cjs'],
+  // parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  // settings: { react: { version: '18.2' } },
+  // plugins: ['react-refresh'],
+  // rules: {
+  //   'react-refresh/only-export-components': [
+  //     'warn',
+  //     { allowConstantExport: true },
+  //   ],
+  // },
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'oliveGreen': "#3D4631",
+        'lightYellow': "#FAFF7E",
+        'lightGreen': "#D1E2C0"
+      },
+      fontFamily: {
+        
+      }
+    },
   },
   plugins: [],
 }
