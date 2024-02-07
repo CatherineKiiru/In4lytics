cb94d89990f8fe67d16b8725c2f8e3640ef2a202
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2024-02-07 21:32:34 +0300

    completed hero section

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -4,20 +4,24 @@ import phoneIcon from "/src/assets/call-icon.svg";
 const Hero = () => {
   return (
     <>
-      <section className="mt-24 text-center flow-root">
-        <h1 className="font-montserrat text-2xl font-bold m-4 text-white">
+      <section className="mt-24 text-center mx-auto flow-root max-w-5xl">
+        <h1 className="font-montserrat text-2xl md:text-7xl font-bold m-4 md:m-6 text-white">
           Elevating digital ideas from meh to magnificent
         </h1>
-        <p className="text-sm font-normal text-white m-4">
+        <p className="text-sm md:text-xl font-normal text-white m-4 md:m-6 px-24">
           Transform and elevate your digital product ideas from simple ideas
           into digital products that captivate through our design and
           development agency.
         </p>
-
-        <button className="mx-auto items-center m-4 px-2 py-4 flex bg-lightYellow rounded-full gap-2">
-          <img className="bg-oliveGreen rounded-full" src={phoneIcon} alt="" />
+        <div className="m-4 md:m-6">
+        <button className="mx-auto items-center   px-6 py-2 md:px-5 md:py-3 flex bg-lightYellow rounded-full gap-2">
+          <img className="bg-oliveGreen rounded-full p-2" src={phoneIcon} alt="" />
           Book a Call
         </button>
+        </div>
+        <h2 className="text-black">
+        Woven with trust, stitched with success
+        </h2>
       </section>
     </>
   );
